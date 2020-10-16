b0f55e293715ff93511d3e38a61a69d5f74eae85
Chondan <chondansusuwan@outlook.com>
2020-10-16 13:02:37 +0700

    uploaded build folder for publishing on github page

---
+++ precache-manifest.4700b301f773e00dfe1d23fd47e52cc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e922d341bc0e9117292c973c9b461b3",
+    "url": "/Youtube-Clone/index.html"
+  },
+  {
+    "revision": "a58252995d25a4b4be67",
+    "url": "/Youtube-Clone/static/css/main.919282fb.chunk.css"
+  },
+  {
+    "revision": "07ab3ab3c527b8d22531",
+    "url": "/Youtube-Clone/static/js/2.49e0a415.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Youtube-Clone/static/js/2.49e0a415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58252995d25a4b4be67",
+    "url": "/Youtube-Clone/static/js/main.6742c6ab.chunk.js"
+  },
+  {
+    "revision": "4921e6adfd6bb5b91c24",
+    "url": "/Youtube-Clone/static/js/runtime-main.4aaf99af.js"
+  }
+]);
